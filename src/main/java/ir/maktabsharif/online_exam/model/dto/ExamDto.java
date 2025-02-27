db0602abd62736cfb2533965bd4cb7d077aeb08e
amir154282 <amirkhp1382@gmail.com>
2025-02-27 05:48:45 +0330

    added

---
+++ src/main/java/ir/maktabsharif/online_exam/model/dto/ExamDto.java
@@ -0,0 +1,34 @@
+package ir.maktabsharif.online_exam.model.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExamDto {
+    @NotBlank(message = "Title cannot be empty")
+    @Size(min = 3, max = 20, message = "Title must be between 3 and 20 characters")
+    private String title;
+
+    @NotBlank(message = "Description cannot be empty")
+    @Size(min = 10, max = 500, message = "Description must be between 10 and 500 characters")
+    private String description;
+
+    @NotNull(message = "Exam time cannot be empty")
+    @Min(value = 30, message = "Exam time must be at least 30 minutes")
+    private Integer examTime;
+
+    @NotNull(message = "Exam date cannot be empty")
+    @Future(message = "Exam date must be in the future")
+    private LocalDate examDate;
+}
+
---
+++ src/main/java/ir/maktabsharif/online_exam/model/dto/UserDto.java
@@ -0,0 +1,41 @@
+package ir.maktabsharif.online_exam.model.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    @NotBlank(message = "First name cannot be empty")
+    @Size(min = 2, max = 30, message = "First name must be between 2 and 30 characters")
+    private String firstName;
+
+    @NotBlank(message = "Last name cannot be empty")
+    @Size(min = 2, max = 30, message = "Last name must be between 2 and 30 characters")
+    private String lastName;
+
+    @NotBlank(message = "Username cannot be empty")
+    @Size(min = 8, max = 20, message = "Username must be between 8 and 20 characters")
+    private String username;
+
+    @NotBlank(message = "Password cannot be empty")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    private String password;
+
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @Past(message = "Date of birth must be in the past")
+    private LocalDate dob;
+}
+
